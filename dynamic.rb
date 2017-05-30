3751745f47d372231d5310b4b392eda97c7cab60
JadziaHax <madelyn.a.thomas@gmail.com>
2017-05-30 12:11:12 -0700

    Add output directory creation, and more console output.
    We now store the emails in a results folder that gets generated if it
    doesn't exit.
    
    We also let the user know how many people were found in the provided
    data and how many emails we generated total.

--- dynamic.rb
+++ dynamic.rb
@@ -1,4 +1,5 @@
 require 'erb'
+require 'fileutils'
 
 # ruby hash notation has allowed a colon (:) after keys instead requiring it
 # before and using a hash rocket since 2.2
@@ -30,9 +31,14 @@ PAYLOAD = {
 
 members = PAYLOAD[:members] # PAYLOAD is a hash and the key :members is an array
                             # of employee objects
+
+puts "Found #{members.length} members for this."
+
+generated = 0
+
 @restaurant = PAYLOAD[:restaurant] # :restaurant key in PAYLOAD is a hash
 
-members.each_with_index do |member, index|
+members.each do |member|
    member_duplicate = members.dup # create a copy of members hash so we don't
                                   # call destructive methods on it because
                                   # the member we email first needs to
@@ -53,11 +59,18 @@ members.each_with_index do |member, index|
    # let the user know we are generating
    puts "Generating email for #{@employee[:name]}..."
 
+   # generate a directory if it doesn't exist
+   dir_name="#{__dir__}/output_emails"
+   unless File.directory?(dir_name)
+     FileUtils.mkdir_p(dir_name)
+   end
+
    # write the results to an html file
-   File.open("result_email_#{@employee[:name]}.html", 'w') do |file|
-     file.write(erb.result)
+   File.open("#{__dir__}/output_emails/result_email_#{@employee[:name]}.html", 'w') {|file| file.write(erb.result)}
+   generated += 1
 
    # let the user know that generation completed
-   puts "Generated result_email_#{@employee[:name]}.html"
-   end
+   puts "output email/Generated result_email_#{@employee[:name]}.html"
 end
+
+puts "Generated #{generated} emails."
